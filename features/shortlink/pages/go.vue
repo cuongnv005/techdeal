12e2d6b57c982b83fd2f287429f2e9612e4c5044
clane-dev3 <cuongnv@bekisoft.com>
2026-08-02 15:33:31 +0700

    fix: open ads

--- features/shortlink/pages/go.vue
+++ features/shortlink/pages/go.vue
@@ -86,14 +86,14 @@ onMounted(() => {
     <Header />
 
     <!-- Banner Adsterra 160x600 — 2 bên, chỉ desktop (xl trở lên). -->
-    <ClientOnly>
+    <!-- <ClientOnly>
       <div v-if="isDesktopAd" class="fixed left-4 top-[150px] z-20">
         <UiAdsterraBanner adKey="e57cbadd5a5a30233c4d746856005893" :width="160" :height="600" />
       </div>
       <div v-if="isDesktopAd" class="fixed right-4 top-[150px] z-20">
         <UiAdsterraBanner adKey="e57cbadd5a5a30233c4d746856005893" :width="160" :height="600" />
       </div>
-    </ClientOnly>
+    </ClientOnly> -->
 
     <main
       class="flex-grow py-20 px-4 flex flex-col items-center justify-center relative overflow-hidden"
@@ -249,14 +249,14 @@ onMounted(() => {
         </div>
 
         <!-- Banner Adsterra giữa — 728x90 desktop, 300x250 mobile -->
-        <ClientOnly>
+        <!-- <ClientOnly>
           <div v-if="isDesktopAd" class="mt-8 flex justify-center w-full relative z-10">
             <UiAdsterraBanner adKey="d27b0d6bc702034be86f143c0b574ec9" :width="728" :height="90" />
           </div>
           <div v-else-if="isMobileAd" class="mt-8 flex justify-center w-full relative z-10">
             <UiAdsterraBanner adKey="de4c283a402db789fc1517d138bbe90b" :width="300" :height="250" />
           </div>
-        </ClientOnly>
+        </ClientOnly> -->
       </div>
     </main>
 
